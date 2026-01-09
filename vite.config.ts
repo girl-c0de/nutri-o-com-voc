b4b38f2fdafc8377f7447b604b164f7cc9b2c747
Aline <alinesantwos13@gmail.com>
2026-01-09 15:09:42 -0300

    feature: agora vai

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/nutri-o-com-voc",
   server: {
     host: "::",
     port: 8080,
